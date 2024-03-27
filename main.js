414af828b34255dd1b2ebd3f4621d50bbceca180
Abubaker Siddiqui <162192538+AbubakerSiddiqui001@users.noreply.github.com>
2024-03-27 06:36:35 +0500

    created whitespace messege

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log('Albert Einsten said, "A person who never made a mistakes never tried anything new"');
+// print messege
+var famous_person = "Albert Einsten";
+var messege = "".concat(famous_person, ", Once said, A person who never made a mistakes never tried anything new");
+console.log(messege);
+// whitespace
+var firstname = ("\t\n Abubaker \t\n");
+console.log(firstname);
+console.log(firstname.trim());
---
+++ main.ts
@@ -0,0 +1,16 @@
+console.log('Albert Einsten said, "A person who never made a mistakes never tried anything new"');
+
+// print messege
+let famous_person: string = "Albert Einsten"
+let messege: string = `${famous_person}, Once said, A person who never made a mistakes never tried anything new`;
+console.log(messege);
+
+// whitespace
+let firstname: string = ("\t\n Abubaker \t\n");
+console.log(firstname); 
+console.log(firstname.trim());
+
+
+
+
+
